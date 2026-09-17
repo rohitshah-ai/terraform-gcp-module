f4c476bbd8cb6b22acea9e7aa9e49ff001c34a19
rohitshah-ai <rohit.shah@nttdata.com>
2026-09-17 15:24:32 +0530

    Disable deletion protection in main.tf

--- cloud-run/main.tf
+++ cloud-run/main.tf
@@ -4,6 +4,7 @@ resource "google_cloud_run_v2_service" "this" {
   location = var.location
 
   ingress = var.ingress
+  deletion_protection = false
 
   template {
     service_account = var.service_account
