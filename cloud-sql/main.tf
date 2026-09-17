2849f6df629d636a78e45c2e295c315a7de81264
Prem-gcp <premkumar.battu@nttdata.com>
2026-09-17 16:24:52 +0530

    Remove edition parameter from Cloud SQL instance
    Removed the edition parameter from the Cloud SQL instance configuration.

--- cloud-sql/main.tf
+++ cloud-sql/main.tf
@@ -3,7 +3,6 @@ resource "google_sql_database_instance" "this" {
   name             = var.instance_name
   database_version = var.database_version
   region           = var.region
-  edition          = var.edition
   settings {
     tier              = var.tier
     availability_type = var.availability_type
