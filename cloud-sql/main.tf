604a2caa1d1f097bf6fd5714cf17e5cf1281b55e
rohitshah-ai <rohit.shah@nttdata.com>
2026-09-18 16:06:46 +0530

    Remove unused Google Cloud SQL resources
    Removed resources for private IP range, VPC connection, database, and user.

--- cloud-sql/main.tf
+++ cloud-sql/main.tf
@@ -1,29 +1,8 @@
-resource "google_compute_global_address" "private_ip_range" {
-  project       = var.project_id
-  name          = "${var.instance_name}-private-ip-range"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = var.private_network
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = var.private_network
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [
-    google_compute_global_address.private_ip_range.name
-  ]
-}
-
 resource "google_sql_database_instance" "this" {
   project          = var.project_id
   name             = var.instance_name
   database_version = var.database_version
   region           = var.cloudsql_region
-
-  depends_on = [
-    google_service_networking_connection.private_vpc_connection
-  ]
 
   settings {
     tier              = var.tier
@@ -44,18 +23,4 @@ resource "google_sql_database_instance" "this" {
   }
 
   deletion_protection = var.deletion_protection
-}
-
-resource "google_sql_database" "this" {
-  project  = var.project_id
-  name     = var.database_name
-  instance = google_sql_database_instance.this.name
-}
-
-resource "google_sql_user" "user" {
-  project  = var.project_id
-  instance = google_sql_database_instance.this.name
-
-  name     = var.database_username
-  password = var.database_password
 }
