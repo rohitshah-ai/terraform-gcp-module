cf23e98f4a823d27a6ba58ac706c155fd0c8ffda
rohitshah-ai <rohit.shah@nttdata.com>
2026-09-21 12:34:05 +0530

    Add dependency on private service access connection

--- cloud-sql/main.tf
+++ cloud-sql/main.tf
@@ -4,6 +4,10 @@ resource "google_sql_database_instance" "this" {
   database_version = var.database_version
   region           = var.cloudsql_region
 
+  depends_on = [
+    google_service_networking_connection.private_service_access
+  ]
+
   settings {
     tier              = var.tier
     edition           = var.edition
